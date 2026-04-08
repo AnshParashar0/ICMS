4365ac0860f1ae279bcdb1f93c8d75332a821b64
Ansh Parashar <parasharansh445@gmail.com>
2026-04-09 02:07:31 +0530

    added root endpoint

---
+++ backend/src/main/java/com/icms/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.icms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "ICMS Backend Running 🚀";
+    }
+} 
